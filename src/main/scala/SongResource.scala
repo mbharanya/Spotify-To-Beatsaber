2f86bdfcce432ee259190487419936031cf87214
mbharanya <spam@trash.li>
2020-01-22 21:59:35 +0100

    Add levenshtein

--- src/main/scala/SongResource.scala
+++ src/main/scala/SongResource.scala
@@ -3,6 +3,7 @@ import java.net.URLEncoder
 import argonaut.Parse
 import argonaut._
 import Argonaut._
+import com.github.vickumar1981.stringdistance.StringDistance.Levenshtein
 
 import scala.io.Source
 import scala.util.Try
@@ -11,18 +12,23 @@ trait SongResource {
   def find(artist: String, song: String): Either[String, SearchResult]
 }
 
-case class SearchResult(name: String, downloads: Long, downloadUrl: String) {
+case class SearchResult(name: String, downloads: Long, downloadUrl: String, confidence: Double) {
 }
 
 
 object BeatsaverSongResource extends SongResource {
   val searchUrl = "https://beatsaver.com/api/search/text/0?q=$SEARCH"
 
-  override def find(artist: String, song: String) = {
-    println(s"Searching for ${artist} ${song}")
+  override def find(inArtist: String, inSong: String) = {
+    println(s"Searching for ${inArtist} ${inSong}")
+
+    val artist = inArtist.toLowerCase
+    val song = inSong.toLowerCase
+
     val result = Try(Source.fromURL(
       searchUrl.replace("$SEARCH", URLEncoder.encode(s"${artist} ${song}", "UTF-8"))
     )).toEither
+
 
     result match {
       case Left(err) => {
@@ -39,12 +45,20 @@ object BeatsaverSongResource extends SongResource {
               name <- (docs --\ "name").focus
               downloads <- (docs --\ "stats" --\ "downloads").focus
               downloadUrl <- (docs --\ "directDownload").focus
-            } yield
+            } yield {
+              val foundSongName = name.stringOrEmpty.toLowerCase
+              val scoreArtistSong = Levenshtein.score(s"${artist} ${song}", foundSongName)
+              val scoreSong = Levenshtein.score(s"${song}", foundSongName)
+
+              val maxScore = Math.max(scoreArtistSong, scoreSong)
+
               SearchResult(
-                name.stringOrEmpty,
+                foundSongName,
                 downloads.numberOrZero.toLong.getOrElse(0l),
-                s"https://beatsaver.com${downloadUrl.stringOrEmpty}"
+                s"https://beatsaver.com${downloadUrl.stringOrEmpty}",
+                maxScore
               )
+            }
 
             maybeSearchResult match {
               case Some(searchResult) => Right(searchResult)
--- src/main/scala/SpotifyToBeatsaber.scala
+++ src/main/scala/SpotifyToBeatsaber.scala
@@ -13,7 +13,7 @@ object SpotifyToBeatsaber {
     implicit val executionCtx = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(1))
 
     val accessToken = Try(args(0)).toOption.getOrElse(throw new IllegalArgumentException("Please add your spotify token as the first argument. Get it from https://developer.spotify.com/console/get-current-user-saved-tracks/"))
-    val downloadFolder = Try(args(1)).toOption.getOrElse("")
+    val downloadFolder = Try(args(1)).toOption.getOrElse(".")
 
     val spotify = new Spotify(accessToken)
 
@@ -27,9 +27,13 @@ object SpotifyToBeatsaber {
       searchResult match {
         case Left(error) => System.err.println(s"Could not find ${track.getArtists()(0).getName} ${track.getName}: ${error}")
         case Right(result) => {
-          println(s"\nFound song '${track.getArtists()(0).getName} ${track.getName}' <-> '${result.name}', ${result.downloads} downloads, ${result.downloadUrl}")
-          val fileName = result.name.replaceAll("\\W+", "-")
-          Downloader.download(result.downloadUrl, s"${downloadFolder}${File.separator}${fileName}.zip")
+          if (result.confidence < 0.4) {
+            System.err.println(s"'${result.name}' is not close enough to '${track.getArtists()(0).getName} ${track.getName}' - ${result.confidence}")
+          }else{
+            println(s"\nFound song '${track.getArtists()(0).getName} ${track.getName}' <-> '${result.name}', ${result.downloads} downloads, ${result.downloadUrl}")
+            val fileName = result.name.replaceAll("\\W+", "-")
+            Downloader.download(result.downloadUrl, s"${downloadFolder}${File.separator}${fileName}.zip")
+          }
         }
       }
     }
