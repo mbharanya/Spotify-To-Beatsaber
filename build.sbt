801b0b1e27378afae4deaa8fbe46a7eff8e09940
Milan Bharanya <milan.bharanya@tundra.com>
2020-01-22 18:19:19 +0100

    Use different download to eliminate jar issues

--- build.sbt
+++ build.sbt
@@ -8,7 +8,6 @@ libraryDependencies += "se.michaelthelin.spotify" % "spotify-web-api-java" % "4.
 libraryDependencies += "org.scala-lang.modules" % "scala-java8-compat_2.12" % "0.9.0"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.0" % "test"
 libraryDependencies += "io.argonaut" %% "argonaut" % "6.2.3"
-libraryDependencies += "commons-io" % "commons-io" % "2.6"
 
 
 resolvers += Resolver.bintrayIvyRepo("sbt", "sbt-plugin-releases")
--- src/main/scala/SpotifyToBeatsaber.scala
+++ src/main/scala/SpotifyToBeatsaber.scala
@@ -1,10 +1,7 @@
 import java.io.File
-import java.net.URL
-import java.nio.file.FileSystem
 import java.util.concurrent.Executors
 
 import com.wrapper.spotify.model_objects.specification.SavedTrack
-import org.apache.commons.io.FileUtils
 
 import scala.concurrent.ExecutionContext
 import scala.util.Try
@@ -26,20 +23,23 @@ object SpotifyToBeatsaber {
       val track = savedTrack.getTrack
       val searchResult = songResource.find(track.getArtists()(0).getName, track.getName)
       searchResult match {
-        case Left(error) => println(s"Could not find ${track.getArtists()(0).getName} ${track.getName}: ${error}")
+        case Left(error) => System.err.println(s"Could not find ${track.getArtists()(0).getName} ${track.getName}: ${error}")
         case Right(result) => {
           println(s"\nFound song '${track.getArtists()(0).getName} ${track.getName}' <-> '${result.name}', ${result.downloads} downloads, ${result.downloadUrl}")
-          downloadFile(result.downloadUrl, filepath = s"${downloadFolder}${File.separator}${result.name}.zip")
+          fileDownloader(result.downloadUrl, s"${downloadFolder}${File.separator}${result.name}.zip")
         }
       }
     }
   }
 
+  import java.io.File
+  import java.net.URL
 
-  def downloadFile(url: String, filepath: String): Unit = {
-    println(s"Will download ${url} to ${new File(filepath).getAbsolutePath}")
-    FileUtils.copyURLToFile(new URL(url), new File(filepath))
-    println("Downloaded " + filepath)
+  import sys.process._
+
+  def fileDownloader(url: String, filename: String) = {
+    new URL(url) #> new File(filename) !!;
+    println(s"Downloaded ${new File(filename).getAbsolutePath}")
   }
 
 
