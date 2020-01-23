95d5bbdc6dc7a0f18cf80c31745140dc61ee9564
Milan Bharanya <milan.bharanya@tundra.com>
2020-01-23 10:38:23 +0100

    Add some stats, terminate correctly

--- src/main/scala/SpotifyToBeatsaber.scala
+++ src/main/scala/SpotifyToBeatsaber.scala
@@ -1,6 +1,4 @@
 import java.io.File
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Paths}
 import java.util.concurrent.Executors
 
 import com.wrapper.spotify.model_objects.specification.SavedTrack
@@ -19,23 +17,42 @@ object SpotifyToBeatsaber {
 
     val songResource: SongResource = BeatsaverSongResource
 
-    for {
+    val statsF = for {
       tracks: List[SavedTrack] <- spotify.findAll()
     } yield tracks.map { savedTrack =>
       val track = savedTrack.getTrack
       val searchResult = songResource.find(track.getArtists()(0).getName, track.getName)
-      searchResult match {
-        case Left(error) => System.err.println(s"Could not find ${track.getArtists()(0).getName} ${track.getName}: ${error}")
+      val matches = searchResult match {
+        case Left(error) => {
+          System.err.println(s"Could not find ${track.getArtists()(0).getName} ${track.getName}: ${error}")
+          (1, 0)
+        }
         case Right(result) => {
           if (result.confidence < 0.4) {
             System.err.println(s"'${result.name}' is not close enough to '${track.getArtists()(0).getName} ${track.getName}' - ${result.confidence}")
+            (1, 0)
           }else{
-            println(s"\nFound song '${track.getArtists()(0).getName} ${track.getName}' <-> '${result.name}', ${result.downloads} downloads, ${result.downloadUrl}")
+            println(s"Found song '${track.getArtists()(0).getName} ${track.getName}' <-> '${result.name}', ${result.downloads} downloads, ${result.downloadUrl}")
             val fileName = result.name.replaceAll("\\W+", "-")
             Downloader.download(result.downloadUrl, s"${downloadFolder}${File.separator}${fileName}.zip")
+            (0,1)
           }
         }
       }
+      println("\n")
+      matches
+    }
+
+    for {
+      stats <- statsF
+    }yield{
+      val (failed, succeeded) = stats.fold((0,0))((a: (Int, Int), b: (Int, Int)) => {
+        ((a._1 + b._1), a._2 + b._2)
+      })
+
+      println(s"✅ Downloaded ${succeeded} / ${succeeded + failed} (${Math.round((succeeded.toDouble / (failed.toDouble + succeeded.toDouble)) * 100)}%)")
+      executionCtx.shutdown()
+      System.exit(0)
     }
   }
 
